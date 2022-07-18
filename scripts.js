a7de20c9927df9fb9445e65b26197c29c3e68d70
Nathan <n.auckett@gmail.com>
2022-07-18 22:25:48 +1000

    Created addDice function. Ready to merge into main for testing on frontend.

--- scripts.js
+++ scripts.js
@@ -6,3 +6,22 @@ function getRoll(_sides){
 function roll(){
     document.getElementById("result").innerHTML = getRoll(6);
 }
+
+function addDice(){
+    const diceDiv = document.createElement("div");
+    diceDiv.className="col"
+    diceDiv.innerHTML = `
+        <div class="border border-5" style="width:16rem; height:16rem;">
+            <h2>Dice 1</h2>
+            <h5>Result:</h5>
+            <h5 id="result">0</h5>
+            <button onclick="roll()">Roll</button>
+        </div><!--Dice div-->`;
+
+    var diceContainer=document.getElementById("diceGrid");
+    diceContainer.appendChild(diceDiv);
+}
+
+function rollAll(){
+
+}
